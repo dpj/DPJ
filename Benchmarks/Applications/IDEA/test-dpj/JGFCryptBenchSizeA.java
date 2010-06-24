145c2ce3b2ceeebf1386e1847335dbae1589541b
Robert Bocchino <bocchino@uiuc.edu>
2010-06-24 01:38:51 -0500

    Add IDEA back to the repo

---
+++ Benchmarks/Applications/IDEA/test-dpj/JGFCryptBenchSizeA.java
@@ -0,0 +1,42 @@
+/**************************************************************************
+*                                                                         *
+*         Java Grande Forum Benchmark Suite - Thread Version 1.0          *
+*                                                                         *
+*                            produced by                                  *
+*                                                                         *
+*                  Java Grande Benchmarking Project                       *
+*                                                                         *
+*                                at                                       *
+*                                                                         *
+*                Edinburgh Parallel Computing Centre                      *
+*                                                                         * 
+*                email: epcc-javagrande@epcc.ed.ac.uk                     *
+*                                                                         *
+*                                                                         *
+*      This version copyright (c) The University of Edinburgh, 2001.      *
+*                         All rights reserved.                            *
+*                                                                         *
+**************************************************************************/
+
+
+//import crypt.*;
+import jgfutil.*; 
+import DPJRuntime.*;
+
+public class JGFCryptBenchSizeA{ 
+
+  public static int nthreads;
+
+  public static void main(String args[]){
+
+      nthreads = RuntimeState.dpjNumThreads;
+
+      JGFInstrumentor.printHeader(2,0,nthreads);
+
+      JGFCryptBench cb = new JGFCryptBench(nthreads); 
+      cb.JGFrun(0);
+ 
+  }
+}
+
+
